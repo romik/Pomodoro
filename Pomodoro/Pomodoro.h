23ee8bc509f4cae62684aeb52e82855e02b9baa0
Romi Khanna <romikhanna@yahoo.co.in>
2014-05-20 11:59:23 +0530

    First

---
+++ Pomodoro/Pomodoro.h
@@ -0,0 +1,32 @@
+
+// Pomodoro.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CPomodoroApp:
+// See Pomodoro.cpp for the implementation of this class
+//
+
+class CPomodoroApp : public CWinApp
+{
+public:
+	CPomodoroApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPomodoroApp theApp;
---
+++ Pomodoro/PomodoroDlg.h
@@ -0,0 +1,40 @@
+
+// PomodoroDlg.h : header file
+//
+
+#pragma once
+
+
+// CPomodoroDlg dialog
+class CPomodoroDlg : public CDialogEx
+{
+// Construction
+public:
+	CPomodoroDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_POMODORO_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+private:
+	UINT_PTR m_nTimer;
+
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
